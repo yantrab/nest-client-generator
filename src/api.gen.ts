73f791fc71830590a647fa188832cc8901dff39d
Yaniv Trabelsi <yantrab@gmail.com>
2019-02-24 20:39:24 +0200

    fix path

--- src/api.gen.ts
+++ src/api.gen.ts
@@ -1,73 +1,75 @@
-import { Project , Scope} from "ts-morph";
+import { Project, Scope } from "ts-morph";
 import { writeFileSync, mkdirSync } from 'fs'
 import * as defualtConfig from './config'
-const sharedFolder = '../shared'
-import {resolve} from 'path'
+import { resolve } from 'path'
 export const startGenerateClientApi = (config = defualtConfig) => {
     const clientPath = resolve(config.clientPath)
     const serverPath = resolve(config.serverPath)
-    import(sharedFolder).then(models => {
+    const modelsPath = resolve(config.modelsPath)
 
-        // Create the client folder with http service file
-        mkdirSync(clientPath, { recursive: true });
-        writeFileSync(clientPath + '/http.service.ts', config.httpServiceTemplate);
+    // Create the client folder with http service file
+    mkdirSync(clientPath, { recursive: true });
+    writeFileSync(clientPath + '/http.service.ts', config.httpServiceTemplate);
 
-        const project = new Project();
-        const files = project.addExistingSourceFiles(serverPath + "/**/*controller.ts");
+    const project = new Project();
+    const models = new Project()
+        .addExistingSourceFiles(modelsPath + "/**/*.ts")
+        .reduce((names, file) => names.concat(file.getClasses().map(c => c.getName())), [])
 
-        files.forEach(file => {
-            const c = file.getClasses()[0];
-            const basePath = c.getDecorator('Controller').getArguments()[0].compilerNode.getText().replace(/'/g, '')
+    const files = project.addExistingSourceFiles(serverPath + "/**/*controller.ts");
 
-            // Remove all class Decorators & add Indectable decorator
-            c.getDecorators().forEach(d => d.remove());
-            c.addDecorator({ name: 'Injectable',arguments:[] })
+    files.forEach(file => {
+        const c = file.getClasses()[0];
+        const basePath = c.getDecorator('Controller').getArguments()[0].compilerNode.getText().replace(/'/g, '')
 
-            // Remove class constrctors & add one with di injectable
-            c.getConstructors().forEach(constructor => constructor.remove())
-            c.addConstructor({ parameters: [{ isReadonly: true, type: 'APIService', name: 'api', scope: Scope.Private}] })
+        // Remove all class Decorators & add Indectable decorator
+        c.getDecorators().forEach(d => d.remove());
+        c.addDecorator({ name: 'Injectable', arguments: [] })
 
-            // Remove all imports but 'shared'
-            file.getImportDeclarations().forEach(i => {
-                if (!i.getChildren().find(c => c.getFullText().includes('shared'))) i.remove()
-            })
+        // Remove class constrctors & add one with di injectable
+        c.getConstructors().forEach(constructor => constructor.remove())
+        c.addConstructor({ parameters: [{ isReadonly: true, type: 'APIService', name: 'api', scope: Scope.Private }] })
 
-            // Add necessary imports
-            file.addImportDeclaration({ namedImports: ['plainToClass'], moduleSpecifier: 'class-transformer' })
-            file.addImportDeclaration({ namedImports: ['APIService'], moduleSpecifier: './http.service' })
-            file.addImportDeclaration({ namedImports: ['Injectable'], moduleSpecifier: '@angular/core' })
-
-            const methods = c.getMethods()
-            methods.forEach(method => {
-                let replacment;
-                const retrunType = method.getReturnType();
-                const returnTypeNode = method.getReturnTypeNode()
-                if (!returnTypeNode) method.setReturnType(retrunType.getText())
-                method.getDecorators().forEach(d => {
-                    const name = d.getName()
-                    if (!config.decorators[name]) return d.remove()
-                    const args = d.getArguments()
-                    const methodPath = args[0] ? args[0].compilerNode.getText().replace(/'/g, '') : ''
-                    replacment =
-                        config.decorators[name]
-                            .replace('{url}', basePath + '/' + methodPath)
-                            .replace('{body}', method.getParameters().filter(p => p.getDecorators().find(d => d.getName() == 'Body')).map(p => p.compilerNode.name.getText()).join(', '))
-                    d.remove()
-                })
-
-                method.getParameters().forEach(p => {
-                    const bodyDecorator = p.getDecorators().find(d => d.getName() == 'Body');
-                    if (!bodyDecorator) return p.remove();
-                    p.getDecorators().forEach(d => d.remove())
-                })
-
-                const implementation = method.getImplementation()
-                const type = method.getReturnTypeNode().getText().replace('Promise<', '').replace('>', '')
-                replacment = replacment.replace('{resolve}', models[type] ? `resolve(plainToClass(${type},<${type}>data))` : 'resolve(data)')
-                implementation.setBodyText(replacment)
-            })
-
-            writeFileSync('client/src/api/' + file.getBaseName(), file.getText())
+        // Remove all imports but 'shared'
+        file.getImportDeclarations().forEach(i => {
+            if (!i.getChildren().find(c => c.getFullText().includes('shared'))) i.remove()
         })
+
+        // Add necessary imports
+        file.addImportDeclaration({ namedImports: ['plainToClass'], moduleSpecifier: 'class-transformer' })
+        file.addImportDeclaration({ namedImports: ['APIService'], moduleSpecifier: './http.service' })
+        file.addImportDeclaration({ namedImports: ['Injectable'], moduleSpecifier: '@angular/core' })
+
+        const methods = c.getMethods()
+        methods.forEach(method => {
+            let replacment;
+            const retrunType = method.getReturnType();
+            const returnTypeNode = method.getReturnTypeNode()
+            if (!returnTypeNode) method.setReturnType(retrunType.getText())
+            method.getDecorators().forEach(d => {
+                const name = d.getName()
+                if (!config.decorators[name]) return d.remove()
+                const args = d.getArguments()
+                const methodPath = args[0] ? args[0].compilerNode.getText().replace(/'/g, '') : ''
+                replacment =
+                    config.decorators[name]
+                        .replace('{url}', basePath + '/' + methodPath)
+                        .replace('{body}', method.getParameters().filter(p => p.getDecorators().find(d => d.getName() == 'Body')).map(p => p.compilerNode.name.getText()).join(', '))
+                d.remove()
+            })
+
+            method.getParameters().forEach(p => {
+                const bodyDecorator = p.getDecorators().find(d => d.getName() == 'Body');
+                if (!bodyDecorator) return p.remove();
+                p.getDecorators().forEach(d => d.remove())
+            })
+
+            const implementation = method.getImplementation()
+            const type = method.getReturnTypeNode().getText().replace('Promise<', '').replace('>', '')
+            replacment = replacment.replace('{resolve}', models.includes(type)? `resolve(plainToClass(${type},<${type}>data))` : 'resolve(data)')
+            implementation.setBodyText(replacment)
+        })
+
+        writeFileSync('client/src/api/' + file.getBaseName(), file.getText())
     })
 }
